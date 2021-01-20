2d65a06ba81fa1c8cc44504a7b1a9c12210211cf
Todd Wolfson <todd@twolfson.com>
2021-01-20 12:49:02 -0800

    Minimized items for what we can

--- src/items/factory.ts
+++ src/items/factory.ts
@@ -1,19 +1,6 @@
-/// <reference path="floor_item.ts" />
-/// <reference path="in_wall_floor_item.ts" />
-/// <reference path="in_wall_item.ts" />
-/// <reference path="on_floor_item.ts" />
-/// <reference path="wall_floor_item.ts" />
-/// <reference path="wall_item.ts" />
-
 module BP3D.Items {
   /** Enumeration of item types. */
   const item_types = {
-    1: Items.FloorItem,
-    2: Items.WallItem,
-    3: Items.InWallItem,
-    7: Items.InWallFloorItem,
-    8: Items.OnFloorItem,
-    9: Items.WallFloorItem
   };
 
   /** Factory class to create items. */
